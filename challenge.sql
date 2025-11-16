ebc1b26cc515cf002126290ba5712f02b3998dfb
Sulenny <sulennyu@usf.edu>
2025-11-16 16:03:53 -0500

    Task 4 extension and note

--- challenge.sql
+++ challenge.sql
@@ -1,3 +1,8 @@
+--Tools used: VSCode + SQLTools (SQLite Viewer)
+--Results validated by executing each query independently 
+--on the bais_sqlite_lab file and confirming the output matched requirements. 
+--Also made sure the SQL ran without errors or crashes. 
+
 -- Task 1 
 SELECT c.first_name || ' ' || c.last_name AS customer_name,
 SUM(oi.quantity * oi.unit_price) AS total_spend
@@ -24,7 +29,7 @@ da.avg
 FROM employees e
 JOIN departments d ON e.department_id = d.id
 JOIN (
-    SELECT department_id, AVG(salary) AS avg
+    SELECT department_id,ROUND(AVG(salary)) AS avg
     FROM employees 
     GROUP BY department_id
 ) AS da ON e.department_id = da.department_id
@@ -33,11 +38,22 @@ ORDER BY d.name, e.salary DESC;
 
 -- Task 4 
 SELECT city,
-COUNT(*) AS gold_customer_count
+COUNT(*) AS gold_c_count
 FROM customers
 WHERE loyalty_level = 'Gold'
 GROUP BY city
-ORDER BY gold_customer_count DESC, city;
+ORDER BY gold_c_count DESC, city;
+
+--TASK 4 (Extension)
+SELECT city,
+SUM(loyalty_level='Gold') AS gold_count,
+SUM(loyalty_level='Silver') AS silver_count,
+SUM(loyalty_level='Bronze') AS bronze_count,
+COUNT(*) AS total_customers
+FROM customers 
+GROUP BY city
+ORDER BY gold_count DESC, city;
+
 
 
 
